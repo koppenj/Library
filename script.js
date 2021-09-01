887e906dc2d81fbce722d5e0f67e71ebfbea2c13
koppenj <joshuwakoppen@gmail.com>
2021-09-01 01:29:26 -0400

    Shaping layout. Adding depencencies. Tooling around with HTML form data.

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
-let myLibrary = [];
+/* const myLibrary = [];
 
-//Constructor for each book
+// Constructor for each book
 function Book(title, author, pageCount, finished) {
-  this.title = title,
-  this.author = author,
-  this.pageCount = pageCount,
-  this.finished = finished,
+  this.title = title;
+  this.author = author;
+  this.pageCount = pageCount;
+  this.finished = finished;
 }
-//Adding a method to Book Object
-Book.prototype.info = () => {
-  return `${title} by ${author}, ${pageCount} pages, ${finished}`
-}
+// Adding a method to Book Object
+Book.prototype.info = () => `${title} by ${author}, ${pageCount} pages, ${finished}`;
 
 const book1 = new Book('The Pragmatic Programmer', 'Andrew Hunt & David Thomas', 320, 'Not Finished');
 
+const bookShelf = document.querySelector('.bookShelf');
 
-function addBookToLibrary(){
-//Exactly what it says, should add the book to myLibrary array
-}
+
+function addBookToLibrary() {
+
+} */
+const form = document.querySelector('#addBook');
+const submit = document.querySelector('#submitBook');
+submit.addEventListener('click', () => {
+  console.log(this.form.title);
+})
 
 
